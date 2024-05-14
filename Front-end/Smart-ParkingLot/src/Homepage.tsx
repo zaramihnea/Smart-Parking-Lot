f47159b23a428158d0f1ee16cfb1849ff31de201
Zara Mihnea <zaramihnea@icloud.com>
2024-05-14 19:10:02 +0300

    Changed searchbar to have dark theme when dark mode is enabled

--- Front-end/Smart-ParkingLot/src/Homepage.tsx
+++ Front-end/Smart-ParkingLot/src/Homepage.tsx
@@ -9,12 +9,14 @@ const Homepage: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
-      <div className="mb-4">
+      <div className="sticky top-0 z-50 bg-gray-100 dark:bg-gray-900">
         <SearchBar />
+      </div>
+      <div className="mb-8">
         <div className="flex justify-between items-center mt-4">
           <h1 className="text-2xl font-semibold">Welcome, username!</h1>
-          <button className="bg-purple-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-purple-700 transition duration-300">
-            Reserve Parking Spot Now
+          <button className="bg-purple-600 text-white px-2 py-2 text-md rounded-lg shadow-md hover:bg-purple-700 transition duration-300 whitespace-nowrap">
+            Reserve Parking Spot
           </button>
         </div>
       </div>
