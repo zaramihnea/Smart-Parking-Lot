8555f3100cdbc6fd907cf88e9a24904b0996346f
Zara Mihnea <zaramihnea@icloud.com>
2024-05-15 22:06:16 +0300

    fix

--- Front-end/Smart-ParkingLot/src/main.tsx
+++ Front-end/Smart-ParkingLot/src/main.tsx
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { registerSW } from 'virtual:pwa-register';
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('Service Worker: Need refresh');
-  },
-  onOfflineReady() {
-    console.log('Service Worker: Offline ready');
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
