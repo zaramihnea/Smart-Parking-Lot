ba40b25edd4238e8393f4cbbd354eac14534a5b9
bluehour19 <105144782+bluehour19@users.noreply.github.com>
2024-05-15 19:39:37 +0300

    Add files via upload
    Account Balance

---
+++ Front-end/Smart-ParkingLot/src/pages/AccountBalance.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const AccountBalance = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [balance, setBalance] = useState(30);
+
+  const handleToggleInput = () => {
+    setShowInput((prevShowInput) => !prevShowInput);
+  };
+
+  const handleAddAmount = () => {
+    if (parseFloat(amount) > 0) {
+      setBalance(balance + parseFloat(amount));
+      setShowInput(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 shadow-lg">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+        <div className="p-4 bg-gray-100 rounded-md mb-4 shadow-lg">
+          <h2 className="text-3xl font-bold text-purple-500 mb-4" style={{ color: '#796bdd' }}>Account Balance</h2>
+          <div className="bg-gray-400 px-4 py-3 rounded-md text-gray-900 mb-4 flex items-center justify-between shadow-lg">
+            <p className="text-4xl font-bold">${balance}</p>
+            <button
+              className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-md shadow-lg hover:bg-gray-300 transition duration-300"
+              onClick={handleToggleInput}
+            >
+              +
+            </button>
+          </div>
+          {showInput && (
+            <div className="p-2 rounded-md mt-4" style={{ width: '40%' }}>
+              <div className="flex items-center">
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  className="flex-grow py-1 px-1 border border-gray-300 rounded-md shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+                <button
+                  onClick={handleAddAmount}
+                  className="bg-d9d9d9 text-gray-800 font-bold py-1 px-1 rounded-md shadow-lg hover:bg-gray-300 transition duration-300"
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          )}
+          <h2 className="text-3xl font-bold text-purple-500 mb-4" style={{ color: '#796bdd' }}>Payment History</h2>
+          <div className="p-4 bg-gray-400 rounded-md shadow-lg">
+            <p className="text-gray-300 font-bold">No registered payments yet..</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountBalance;
