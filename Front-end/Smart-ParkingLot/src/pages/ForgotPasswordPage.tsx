e7e2ae8d8f0e4776620d9cd55e4a80b193dc2731
roxanaradu20 <radu.roxana.72@gmail.com>
2024-05-28 23:52:16 +0300

    edited loginPage, forgotPasswordPage and created ResetPasswordPage

--- Front-end/Smart-ParkingLot/src/pages/ForgotPasswordPage.tsx
+++ Front-end/Smart-ParkingLot/src/pages/ForgotPasswordPage.tsx
@@ -3,20 +3,58 @@ import { useNavigate } from 'react-router-dom';
 
 const ForgotPasswordPage: React.FC = () => {
   const navigate = useNavigate();
-
   const [email, setEmail] = useState('');
   const [emailErrorPopup, setemailErrorPopup] = useState(false);
+  const [errorMessage, setUIErrorMessage] = useState('');
 
-  const handleFP = () => {
+  const [emailSentBool, setemailSentBool] = useState(false);
+  const [emailSentMessage, setemailSentMessage] = useState('An email has been successfully sent');
+  const baseUrl = process.env.API_BASE_URL;
+
+  const handleFP = async () => {
     if (isValidEmail(email)) {
       console.log("Sending data to backend for forgot password:", email);
-      setemailErrorPopup(false);
-      navigate('/');
+      try {
+        const response = await fetch(`${baseUrl}/user/reset-password-request`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email
+            }),
+        });
+
+        if (response.ok) {
+          const textResponse = await response.text();
+          if(textResponse === 'Email found'){
+            setemailSentBool(true);
+            setemailErrorPopup(false);
+            
+            navigate('/');
+            
+          }
+
+        }
+        else {
+          const textResponse = await response.text();
+          setemailSentBool(false);
+          setUIErrorMessage('Email not found');
+          setemailErrorPopup(true);
+        }
+      } catch (error) {
+        setemailSentBool(false);
+        console.error("Error during password reset:", error);
+        setUIErrorMessage('An unexpected error occurred. Please try again.');
+        setemailErrorPopup(true);
+      }
     } else {
+      setemailSentBool(false);
+      setUIErrorMessage('Email is invalid');
       setemailErrorPopup(true);
     }
 
-  }
+  };
 
   const isValidEmail = (email: string) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -36,7 +74,8 @@ const ForgotPasswordPage: React.FC = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="w-full max-w-xs px-4 py-2 mb-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm focus:outline-none focus:border-purple-500"
           />
-          {emailErrorPopup && <p className="text-red-500 text-sm mb-4">Email is invalid!</p>}
+          {emailErrorPopup && <p className="text-red-500 text-center mb-4">{errorMessage}</p>}
+          {emailSentBool && <p className="text-green-500 text-center mb-4">{emailSentMessage}</p>}
           <button
             onClick={handleFP}
             className="w-full max-w-xs px-4 py-2 mb-4 bg-purple-600 text-white font-bold rounded-lg shadow-md hover:bg-purple-700 transition duration-300"
--- Front-end/Smart-ParkingLot/src/pages/LoginPage.tsx
+++ Front-end/Smart-ParkingLot/src/pages/LoginPage.tsx
@@ -13,8 +13,6 @@ const LoginPage: React.FC = () => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false); // Stare pentru a afișa parola
 
-  const [loginErrorPopup, setloginErrorPopup] = useState(false);
-  const [emailErrorPopup, setemailErrorPopup] = useState(false);
   const [errorMessage, setUIErrorMessage] = useState('');
   const baseUrl = process.env.API_BASE_URL;
 
@@ -30,56 +28,58 @@ const LoginPage: React.FC = () => {
   // }, [navigate]); // Add 'navigate' as a dependency
 
   const handleLogin = async () => {
-    if (isValidEmail(email)) {
-      if (password) {
+    if(email === '' && password === ''){
+      setUIErrorMessage('Please enter your email address and your password');
+    } else if(email === ''){
+      setUIErrorMessage('Please enter your email address');
+    } else if(password === ''){
+      setUIErrorMessage('Please enter your password');
+    } else {
+      if (isValidEmail(email)) {
           console.log("Sending data to backend for login:", email, password);
           try {
-              const response = await fetch(`${baseUrl}/user/login`, {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({
-                      email: email,
-                      password: password
-                  }),
-              });
+            const response = await fetch(`${baseUrl}/user/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                email: email,
+                password: password
+              }),
+            });
 
-              console.log(JSON.stringify({
-                  email: email,
-                  password: password
-              }));
+            console.log(JSON.stringify({
+              email: email,
+              password: password
+            }));
 
-              if (response.ok) {
-                  const data = await response.json(); // Assuming the response contains JSON data
-                  console.log("Login successful:", data);
+            if (response.ok) {
+              const data = await response.json(); // Assuming the response contains JSON data
+              console.log("Login successful:", data);
 
-                  // Assuming the response contains a token
-                  const { token } = data;
-                  
-                  // Set the cookie using document.cookie
-                  document.cookie = `authToken=${token}; path=/; max-age=3600; SameSite=Strict; Secure`;
+              // Assuming the response contains a token
+              const { token } = data;
+                    
+              // Set the cookie using document.cookie
+              document.cookie = `authToken=${token}; path=/; max-age=3600; SameSite=Strict; Secure`;
 
-                  // Or set the cookie using js-cookie
-                  // Cookies.set('authToken', token, { expires: 1, path: '/', sameSite: 'Strict', secure: true }); // Expires in 1 day
+              // Or set the cookie using js-cookie
+              // Cookies.set('authToken', token, { expires: 1, path: '/', sameSite: 'Strict', secure: true }); // Expires in 1 day
 
-                  navigate('/home');
-              } else {
-                  const errorData = await response.text();
-                  console.error("Login failed:", errorData);
-                  setUIErrorMessage(errorData);
-              }
+              navigate('/home');
+            } else {
+              const errorData = await response.text();
+              setUIErrorMessage(errorData);
+            }
           } catch (error) {
-              console.error("Error during Login:", error);
-              setUIErrorMessage('An unexpected error occurred. Please try again.');
+            console.error("Error during Login:", error);
+            setUIErrorMessage('An unexpected error occurred. Please try again.');
           }
-      } else {
-          setloginErrorPopup(true);
-      }
-  } else {
-      setemailErrorPopup(true);
-  }
-
+      }else {
+          setUIErrorMessage('Invalid email');
+        }
+    }
   };
 
   const isValidEmail = (email: string) => {
@@ -100,7 +100,6 @@ const LoginPage: React.FC = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="w-full max-w-xs px-4 py-2 mb-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm focus:outline-none focus:border-purple-500"
           />
-          {emailErrorPopup && <p className="text-red-500 text-sm mb-4">Email is invalid!</p>}
           <div className="relative w-full max-w-xs">
             <input
               type={showPassword ? "text" : "password"}
@@ -117,8 +116,7 @@ const LoginPage: React.FC = () => {
               <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
             </button>
           </div>
-          {loginErrorPopup && <p className="text-red-500 text-sm mb-4">Please fill in all fields</p>}
-          {errorMessage && <div className="text-red-500 mt-4">{errorMessage}</div>} {/* Display the error message */}
+          <div className="text-red-500 text-center mb-4">{errorMessage}</div> {/* Display the error message */}
           <button
             onClick={handleLogin}
             className="w-full max-w-xs px-4 py-2 mb-4 bg-purple-600 text-white font-bold rounded-lg shadow-md hover:bg-purple-700 transition duration-300"
